595f5864778778e50c08beb517a225f08139b708
johnmaxnew <forevermeticulous@gmail.com>
2021-10-14 12:47:25 +0300

    created api

--- drf_auth/urls.py
+++ drf_auth/urls.py
@@ -20,9 +20,17 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from rest_framework.routers import DefaultRouter
+from alpha.views import UserViewSet
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
+router = DefaultRouter()
+router.register('user', UserViewSet, basename='user')
+
+urlpatterns+=router.urls
