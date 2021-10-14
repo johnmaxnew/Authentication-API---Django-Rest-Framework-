9151d1a4fa4e60943b9cfd134c6d4c88b9e48625
johnmaxnew <forevermeticulous@gmail.com>
2021-10-14 13:27:33 +0300

    changed settings

--- drf_auth/settings.py
+++ drf_auth/settings.py
@@ -9,6 +9,18 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+import os
+
+
+import environ
+env = environ.Env(
+# set casting, default value
+DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+
 
 from pathlib import Path
 
@@ -20,12 +32,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#z8==c1xh3cdsy5rk)^&cv%qrohtun4k0$p-bb1il2vylvfuw'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
